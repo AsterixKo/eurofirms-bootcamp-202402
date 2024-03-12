07ba966a0aef262fc1db6714a515c12bea5fb28e
Javier Moneo <jvr.moneo@gmail.com>
2024-03-12 13:08:33 +0100

    correct partial split; #141

--- staff/javier-moneo/strings/split.js
+++ staff/javier-moneo/strings/split.js
@@ -1,22 +1,27 @@
-function split(str, separator, limit) {
+// TODO: esta implementación solo tiene en cuenta cuando solo tiene un caracter el separador
+// TODO: corregir lo del limit, el limit debería ser la longitud del array a devolver
+function split(string, separator, limit) {
   if (limit === 0) {
     return [];
   }
-  var limitCalculated = str.length;
+
+  var limitCalculated = string.length;
+
   if (limit !== undefined && Number(limit) !== NaN && limit > 0) {
     limitCalculated = limit;
   }
 
-  var result = [];
+  var result = []
   var j = 0;
+
   for (var i = 0; i < limitCalculated; i++) {
-    if (str[i] !== separator && separator === '') {
-      result[j] = str[i];
-      j++;
-    } else if (str[i] !== separator && result[j] === undefined) {
-      result[j] = '' + str[i];
-    } else if (str[i] !== separator) {
-      result[j] += str[i];
+    // if (str[i] !== separator && separator === '') {
+      if (separator === '') {
+      result[result.length] = string[i];
+    } else if (string[i] !== separator && result[j] === undefined) {
+      result[j] = string[i];
+    } else if (string[i] !== separator) {
+      result[j] += string[i]; 
     } else if (result[j].length > 0) {
       j++;
     }
@@ -29,18 +34,18 @@ console.log('------------------SPLITE----------------');
 const str = 'The quick brown fox jumps over the lazy dog.';
 
 const words = split(str, ' ');
-// console.log({ words: words });
-console.log({ words3: words[3] });
+console.log({ words: words });
+// console.log({ words3: words[3] });
 // Expected output: "fox"
 
 const wordsCase2 = split(str, ' ', 5);
-// console.log({ wordsCase2: wordsCase2 });
-console.log({ wordsCase2: wordsCase2[1] });
+console.log({ wordsCase2: wordsCase2 });
+// console.log({ wordsCase2: wordsCase2[1] });
 // Expected output: "q"
 
 const chars = split(str, '');
-// console.log({ chars: chars });
-console.log({ chars8: chars[8] });
+console.log({ chars: chars });
+// console.log({ chars8: chars[8] });
 // Expected output: "k"
 
 const strCopy = split(str);
